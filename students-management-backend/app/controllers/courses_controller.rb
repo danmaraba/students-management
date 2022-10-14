7b41bf121a0f49e4d0ddaa5f1cfcc6e55b566e8a
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-14 18:42:02 +0300

    added action methods to courses controller

--- students-management-backend/app/controllers/courses_controller.rb
+++ students-management-backend/app/controllers/courses_controller.rb
@@ -1,2 +1,19 @@
 class CoursesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_not_found_response
+    def index
+        courses = Course.all
+        render json: courses
+    end
+    def create
+        course = Course.create!(params_course)
+        student = Student.find(course.student_id)
+        render json: student, status: :created
+    end
+    private
+    def params_course
+        params.permit(:department, :student_id, :tutor_id)
+    end
+    def render_not_found_response
+        render json: { error: ["validation errors"] }, status: :unprocessable_entity
+    end
 end
