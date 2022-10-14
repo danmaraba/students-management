0041fd45f54a7e8f958c9aabd2751ae838d1633e
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-14 18:43:01 +0300

    added action methods to sessions controller

--- students-management-backend/app/controllers/sessions_controller.rb
+++ students-management-backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+    def create
+        student = Student.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+          session[:student_id] = student.id
+          render json: student, status: :created
+        else
+          render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+    def destroy
+        session.delete :student_id
+        head :no_content
+    end
 end
