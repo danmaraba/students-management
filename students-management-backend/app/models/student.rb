fbd64c6e581ef5546306c193c592de6d4b96c023
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-14 18:31:54 +0300

    Created relationships for the class student

--- students-management-backend/app/models/student.rb
+++ students-management-backend/app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ApplicationRecord
+    has_many :courses
+    has_many :tutors,through: :courses
+    has_secure_password
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :username, presence: true, uniqueness: true
+    validates :password,
+              length: { minimum: 6 },
+              if:  -> { new_record? || !password.nil? }
 end
