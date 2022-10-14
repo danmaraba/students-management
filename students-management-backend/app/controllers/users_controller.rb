394959a69aa6f5eaea6a9ada04a56f1a94109b03
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-14 18:46:17 +0300

    added action methods to users controller

--- students-management-backend/app/controllers/users_controller.rb
+++ students-management-backend/app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
+    def create
+        student = Student.create(student_params)
+        if student.valid?
+            session[:student_id] = student.id
+            render json: student, status: :created
+        else
+            render json: { error: student.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    def show
+        student = Student.find_by(id: session[:student_id])
+        render json: student
+    end
+    private
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :student_id
+    end
+
+    def student_params
+        params.permit(:username, :password, :password_confirmation)
+    end
 end
